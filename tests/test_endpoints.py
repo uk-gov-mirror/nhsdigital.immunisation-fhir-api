5c03361d545e0803d5f646fe43fe3047b8965e25
root <root@LAPTOP-S33IKCKK>
2023-11-10 10:07:08 +0000

    Add whitespace

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -3,6 +3,7 @@ See
 https://github.com/NHSDigital/pytest-nhsd-apim/blob/main/tests/test_examples.py
 for more ideas on how to test the authorization of your API.
 """
+
 from os import getenv
 
 import pytest
